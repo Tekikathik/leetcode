05280625074936e5eaf2f2eb4f35cba4c8e36509
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2024-10-07 12:14:24 +0530

    Time: 9 ms (24.64%), Space: 11.7 MB (18.56%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(int i=0; i<s.size(); i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty() && st.top()=='A' && s[i]=='B'){
+                    st.pop();
+                }
+                else if(!st.empty() && st.top()=='C' && s[i]=='D'){
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans.size();
+    }
+};
