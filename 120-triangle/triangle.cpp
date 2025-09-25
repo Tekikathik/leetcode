53511322a1ca2e03ae0edb0a1f92f58769595f58
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2025-09-25 19:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (81.37%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,14 +3,7 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         for(int i=triangle.size()-2; i>=0 ;i--){
             for (int j=0;j<triangle[i].size();j++){
-                // cout << triangle[i+1][j] << endl;
-                // if (j==triangle[i].size()-1) triangle[i][j]+=triangle[i+1][j];
-                if (triangle[i+1][j] < 0 && triangle[i+1][j+1] < 0) {
-                    triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
-                    // cout << max(triangle[i+1][j],triangle[i+1][j+1]) << endl;
-                } 
-                else triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
-                // cout << triangle[i][j] << endl;
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
             }
         }
         return triangle[0][0];
