68f95761ea9d06866277295c8371dfe8ae8e267f
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-01-04 21:00:15 +0530

    Time: 3 ms (43.68%) | Memory: 47.4 MB (9.83%) - LeetSync

---
+++ 979-di-string-match/di-string-match.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int I=0,d=s.length();
+        int[] arr=new int[s.length()+1];
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='D') arr[i]=d--;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) arr[i]=I++;
+        }
+        return arr;
+    }
+}
