187a3cab09779e4873266a15858e2311b4906a78
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-02-18 22:48:45 +0530

    Time: 66 ms (32.85%), Space: 47.7 MB (28.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -10,10 +10,10 @@ public:
             if (mp.find(sum-k)!=mp.end()){
                 c+=mp[sum-k];
             }
-            mp[sum-k]++;
+            mp[sum]++;
             
         }
-        return 0;
+        return c;
         
     }
 };
