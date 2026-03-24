74734895172d4f180f01b0652f229ca7608d2dd1
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-03-24 21:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.92%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // void cmp()
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.size();i++) {
+            if (nums[i]!=val) {
+                nums[k++]=nums[i];
+            }
+        }
+        return k;
+    }
+};
