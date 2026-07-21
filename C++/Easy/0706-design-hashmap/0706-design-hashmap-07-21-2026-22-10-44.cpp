e5a5b7175cab0b24324898de793a5003daeb26ba
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-07-21 22:10:47 +0530

    Time: 67 ms (31.38%), Space: 220.4 MB (11.43%) - LeetHub

---
+++ C++/Easy/0706-design-hashmap/0706-design-hashmap-07-21-2026-22-10-44.cpp
@@ -0,0 +1,20 @@
+class MyHashMap {
+public:
+    vector<int> data;
+
+    MyHashMap() {
+        data.resize(1000001, -1);
+    }
+
+    void put(int key, int value) {
+        data[key] = value;
+    }
+
+    int get(int key) {
+        return data[key];
+    }
+
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
