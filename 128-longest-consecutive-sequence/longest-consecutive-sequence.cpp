992f277682cec1fe530936806aadd4459b4e75f3
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-07-22 13:50:30 +0530

    Time: 75 ms (75.10%) | Memory: 65.5 MB (77.87%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> v;
-        for(int i=0;i<nums.size();i++){
-            v.insert(nums[i]);
+        if (nums.size()==0) return 0;
+        priority_queue<int> pq;
+        for (int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
         }
-        long long ma=0;
-        long long j=0;
-        for(auto i:v){
-            if (v.find(i-1)==v.end()){
-                long long c=0;
-                for(int j=i;;j++){
-                    if (v.find(j)==v.end()){
-                        ma=max(ma,c);
-                        c=0;
-                        break;
-                    }
-                    else c++;
-                }
-            } 
+        int c=1;
+        int a=pq.top();
+        int ma=INT_MIN;
+        pq.pop();
+        while (!pq.empty()){
+            // cout << pq.top() << endl;
+            // cout << a << endl;
+            if (pq.top()==a){
+                a=pq.top();
+                pq.pop();
+                continue;
+            }
+            if (pq.top()==(a-1)){
+                c++;
+            }
+            else c=1;
+            ma=max(ma,c);
+            a=pq.top();
+            pq.pop();
         }
+        if (ma==INT_MIN) return 1;
         return ma;
     }
 };
