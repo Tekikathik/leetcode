f6970427d0071878057a2c4dfedf3c9306672148
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-07-27 10:43:05 +0530

    Time: 3 ms (65.76%), Space: 10 MB (70.85%) - LeetHub

---
+++ C++/Medium/0567-permutation-in-string/0567-permutation-in-string-07-27-2026-10-43-05.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.size()<s1.size()) return false;
+        vector<int> vv(26,0);
+        vector<int> v(26,0);
+        // if (v) return 
+        for (int i=0;i<s1.size();i++){
+            v[s1[i]-'a']++;
+            vv[s2[i]-'a']++;
+        }
+        // for (int i=0;i<26;i++){
+        //     cout << v[i] << " " << vv[i] << endl;
+        // }
+        int i=1;
+        int j=s1.size();
+        while (i<s2.size() && j<s2.size()){
+            if (v==vv) return true;
+            vv[s2[i-1]-'a']--;
+            vv[s2[j]-'a']++;
+            j++;
+            i++;
+        }
+        if(v==vv) return true;
+        return false;
+    }
+};
