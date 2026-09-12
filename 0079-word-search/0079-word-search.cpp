516a62517b95f47552c3b2047beddaff2fa65910
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-09-12 18:13:14 +0530

    Time: 831 ms (20.1%), Space: 11.5 MB (37.59%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(vector<vector<char>>& board,int r,int c,int d,string word,int n){
+        if (d==n){
+            return true;
+        }
+        if (r<0 || c<0 || r==board.size() || c==board[0].size()|| board[r][c]!=word[d]){
+            return false;
+        }
+        char ch=board[r][c];
+        board[r][c]=' ';
+
+        bool ans=solve(board,r,c-1,d+1,word,n) || solve(board,r,c+1,d+1,word,n) || solve(board,r-1,c,d+1,word,n) || solve(board,r+1,c,d+1,word,n);
+        board[r][c]=ch;
+        return ans;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if (solve(board,i,j,0,word,word.size())) return true;
+            }
+        }
+        return false;
+    }
+};
